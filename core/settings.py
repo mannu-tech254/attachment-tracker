91897fc41990d182d68e820639e3a1a65cb819bf
mannu-tech254 <oigoroemmanuel6@gmail.com>
2026-06-09 08:50:57 +0300

    Add PostgreSQL database support

--- core/settings.py
+++ core/settings.py
@@ -127,7 +127,10 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 DATABASE_URL = os.environ.get('DATABASE_URL')
 if DATABASE_URL:
     DATABASES['default'] = dj_database_url.parse(DATABASE_URL)
-    
+    import dj_database_url
 import os
+DATABASE_URL = os.environ.get('DATABASE_URL')
+if DATABASE_URL:
+    DATABASES['default'] = dj_database_url.parse(DATABASE_URL)
 from django.db.models.signals import post_migrate
 from django.dispatch import receiver
